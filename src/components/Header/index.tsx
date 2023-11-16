79abc1c050d5e154e048f3f318a4ee6b90dd4f2a
diegoandcontroll <lukasxdp2@gmail.com>
2023-11-16 17:09:33 -0300

    feat: add animations use framer motion

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,33 @@
 import React from "react";
-import { HeaderContainer, NavContainer, Logo } from "./styles";
+import { motion } from "framer-motion";
+import {
+  HeaderContainer as StyledHeaderContainer,
+  NavContainer as StyledNavContainer,
+  Logo as StyledLogo,
+} from "./styles";
 
 import SideBar from "../SideBar";
 
 export const Header = () => {
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { duration: 1 } },
+  };
+
+  const logoVariants = {
+    hidden: { opacity: 0, y: -50 },
+    visible: { opacity: 1, y: 0, transition: { duration: 1, delay: 0.5 } },
+  };
+
   return (
-    <HeaderContainer>
-      <NavContainer>
-        <Logo>
+    <StyledHeaderContainer as={motion.div} variants={containerVariants} initial="hidden" animate="visible">
+      <StyledNavContainer as={motion.div}>
+        <StyledLogo as={motion.div} variants={logoVariants} initial="hidden" animate="visible">
           <span>MKS</span>
           <h1>Sistemas</h1>
-        </Logo>
+        </StyledLogo>
         <SideBar cartOpen />
-      </NavContainer>
-    </HeaderContainer>
+      </StyledNavContainer>
+    </StyledHeaderContainer>
   );
 };
